e6450bbe04997eb94ee08befa46a366949b2f9c4
depuc <koripallipraveendeepak@gmail.com>
2025-07-08 20:11:35 +0530

    Added a new Class:(GitRepository)

--- vc.py
+++ vc.py
@@ -60,9 +60,39 @@ def main(argv=sys.argv[1:]):
         
         case "tag" : cmd_tag(args)
 
-        case _     : print("Bad command.") 
+        case _  : print("Bad command.") 
     
     
-    
+class GitRepository(object):
+
+    worktree = None;
+    conf = None;
+    gitdir = None;
+
+    def __init__(self,path,force = False):
+        self.worktree = path 
+        self.gitdir = os.path.join(path,".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
+
+        self.conf = configparser.ConfigParser() 
+        cf = repo_file(self,"config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception(f"Configuration file missing")
+
+        if not force:
+            vers = int(sef.conf.get("core","repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryfarmatverion: {vers}")
+        
+        
+        
+
+
+
     
 
